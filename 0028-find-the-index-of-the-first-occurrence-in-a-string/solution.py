20bea6f366cd41e19f9fc09a2e1c418a0356e382
bhanuprakash7070 <keshavagaribhanuprakash@gmail.com>
2026-03-25 09:44:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (8.79%)

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle in haystack:
+            a=haystack.index(needle)
+            return a
+        else:
+            return -1 
