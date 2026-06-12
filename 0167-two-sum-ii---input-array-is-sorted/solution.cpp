bd5b503c4aa76a81e75b3144eefe5613e29fef53
bhanuprakash7070 <keshavagaribhanuprakash@gmail.com>
2026-06-12 07:01:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (32.97%)

---
+++ 0167-two-sum-ii---input-array-is-sorted/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int>ans;
+        int l=0;
+        int r=numbers.size()-1;
+        while(l<r){
+            int s = numbers[l] + numbers[r];
+            if(s==target){
+                return {l+1,r+1};
+            }
+            else if(s<target){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return {-1,-1};
+    }
+};
